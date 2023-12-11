bdd1de63467fd438ca21feece4acadcdd0bf45ee
Sagaz0501 <sanbernale@gmail.com>
2023-12-11 12:36:16 -0500

    cosas basicas JS

---
+++ valores.js
@@ -0,0 +1,55 @@
+//tipos de valores//
+
+40 /*valor numerico*/
+
+"texto"/* valor de texto*/
+
+true
+false /*bulianos true=1 false=0*/
+
+null
+undefined /*valores vacios, place holder*/
+
+[1,2,3] //valor tipo objeto//
+{nombre: "SANTIAGO"} //valor tipo objeto//
+
+
+
+
+
+//tipos de variables//
+
+var name = "SANTIAGO"; //reserva un espacio en la memoria para guardar un valor en este caso un nombre//
+
+//declarar//
+var age; 
+
+age
+//inicializar//
+age = 30;
+
+age
+
+var elements = ["computadore", "celular"]
+
+var people = {nombre: "SANTIAGO", edad: 30}
+
+
+
+//funciones
+
+//funciones declarativas//
+
+function miFunction(){
+    return 3;
+}
+
+miFunction() //como se manda a llamar la funcion
+
+//funciones de expresion//
+
+var miFunction = function(a, b){
+    return a + b;
+} //una funcion que esta dentro de una variable, //la funcion no tiene nombre
+
+miFunction() //como se manda a llamar una funcion expresiva, que es una varaible que funciona como function
